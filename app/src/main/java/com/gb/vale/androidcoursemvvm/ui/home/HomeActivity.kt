ea2e023e29f7e51eef58ff8367d14917dcc113c8
tayler <emontesinos812@gmail.com>
2023-05-14 08:57:46 -0500

    add librari movie

--- app/src/main/java/com/gb/vale/androidcoursemvvm/ui/home/HomeActivity.kt
+++ app/src/main/java/com/gb/vale/androidcoursemvvm/ui/home/HomeActivity.kt
@@ -5,17 +5,21 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.gb.vale.androidcoursemvvm.R
 import com.gb.vale.androidcoursemvvm.databinding.ActivityHomeBinding
 import com.gb.vale.androidcoursemvvm.databinding.ActivityLoginBinding
 import com.gb.vale.androidcoursemvvm.ui.AppViewModel
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private var shared : SharedPreferences? = null
+    private var idVideo: String? = null
     companion object{
         fun newInstance(context: Context) = context.startActivity(Intent(context, HomeActivity::class.java))
     }
@@ -33,6 +37,15 @@ class HomeActivity : AppCompatActivity() {
            shared?.edit()?.putBoolean("token",false)?.apply()
             finish()
         }
+
+        lifecycle.addObserver(binding.youtubePlayerView)
+        binding.youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(@NonNull youTubePlayer: YouTubePlayer) {
+                val videoId = idVideo
+                videoId?.let { youTubePlayer.loadVideo(it, 0f) }
+                youTubePlayer.pause()
+            }
+        })
 
     }
 
