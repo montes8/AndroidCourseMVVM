29e29084aa6be204419723284cf000d71739c16c
tayler <emontesinos812@gmail.com>
2023-05-29 08:33:06 -0500

    update url

--- app/src/main/java/com/gb/vale/androidcoursemvvm/utils/Constant.kt
+++ app/src/main/java/com/gb/vale/androidcoursemvvm/utils/Constant.kt
@@ -1,4 +1,4 @@
 package com.gb.vale.androidcoursemvvm.utils
 
-const val URL_BASE = "https://3c61-2800-200-e3c0-1123-a4e4-d430-3964-c743.ngrok-free.app/"
+const val URL_BASE = "https://431a-2800-200-e3c0-1123-5d9-8dae-1dd2-5b7d.ngrok-free.app/"
 const val EMPTY = ""
