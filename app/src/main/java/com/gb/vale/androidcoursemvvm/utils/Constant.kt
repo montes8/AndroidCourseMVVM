9c1382f14140e1cd3c5aeb5d42a106c66d6ea7bb
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-07-14 20:42:23 -0500

    update url

--- app/src/main/java/com/gb/vale/androidcoursemvvm/utils/Constant.kt
+++ app/src/main/java/com/gb/vale/androidcoursemvvm/utils/Constant.kt
@@ -2,7 +2,7 @@ package com.gb.vale.androidcoursemvvm.utils
 
 const val EMPTY = ""
 const val PREFERENCE_TOKEN = "PREFERENCE_TOKEN"
-const val URL_BASE = "https://3c61-2800-200-e3c0-1123-a4e4-d430-3964-c743.ngrok-free.app/"
+const val URL_BASE = "https://8236-2800-200-e3c0-1123-ec7d-fca6-2a9c-85d1.ngrok-free.app/"
 const val MY_TIME_ON = 60L
 
 const val KEY_PREFERENCE = "ACMPreferences"
