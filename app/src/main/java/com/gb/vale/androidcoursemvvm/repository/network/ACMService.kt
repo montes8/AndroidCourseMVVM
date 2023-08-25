9588920180fd4db4515e2f98b2170fe0fc74e723
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-08-25 08:21:14 -0500

    update url

--- app/src/main/java/com/gb/vale/androidcoursemvvm/repository/network/ACMService.kt
+++ app/src/main/java/com/gb/vale/androidcoursemvvm/repository/network/ACMService.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ACMService {
-    @GET("api/user/loadMovi")
+    @GET("service/user/loadMovi")
      suspend fun loadMovie(): Response<MovieResponse>
 
 }
