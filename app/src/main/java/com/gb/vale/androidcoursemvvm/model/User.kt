bdd044162dbf31d1486006c5c910f528385a5a2c
eymontesinos <135139454+eymontesinos@users.noreply.github.com>
2023-11-07 09:54:04 -0500

    update plugin parcelize

--- app/src/main/java/com/gb/vale/androidcoursemvvm/model/User.kt
+++ app/src/main/java/com/gb/vale/androidcoursemvvm/model/User.kt
@@ -2,7 +2,7 @@ package com.gb.vale.androidcoursemvvm.model
 
 import android.os.Parcelable
 import com.gb.vale.androidcoursemvvm.utils.EMPTY
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class User(
